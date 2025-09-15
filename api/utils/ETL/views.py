7e33e6191803665ea5aac2830a2a1f3d833d2074
Ho-Ya-Ho <rkdgh1056@gmail.com>
2025-09-15 12:24:13 +0900

    get_transaction_data() api 제작 완

--- api/utils/ETL/views.py
+++ api/utils/ETL/views.py
@@ -6,5 +6,8 @@ from api.utils.etl.services import get_transaction_trace
 
 @api_view(['GET'])
 def get_transaction_data(request):
-    result = get_transaction_trace()
+    start_block = request.query_params.get("start_block")
+    end_block = request.query_params.get("end_block")
+
+    result = get_transaction_trace(start_block, end_block)
     return Response(result)
